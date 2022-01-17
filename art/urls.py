51a060450d618caaabececf4d0d229deb9d80819
Nima <nimiologyy@gmail.com>
2022-01-17 22:46:10 +0330

    changing api code place

--- art/urls.py
+++ art/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
 
+from art.views import GetAllArtsAPI, GetARandomArtAPI, GetArtAPI, GetARandomArtPictureAPI
+
 urlpatterns = [
-    path('api/', include('art.api.urls'))
+    path('', GetAllArtsAPI.as_view()),
+    path('<int:pk>/', GetArtAPI.as_view()),
+    path('random/', GetARandomArtAPI.as_view()),
+    path('random/picture/', GetARandomArtPictureAPI.as_view()),
 ]
--- art/views.py
+++ art/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+from rest_framework.generics import RetrieveAPIView, ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from art.serializer import ArtSerializer
+from art.models import Art
+
+
+class GetAllArtsAPI(ListAPIView):
+    serializer_class = ArtSerializer
+    queryset = Art.objects.all().order_by('?')
+    filterset_fields = ['painter', 'title', 'year', 'type', 'location']
+    ordering_fields = ['id','painter', 'title', 'year', 'type', 'location', '?']
+
+
+class GetArtAPI(RetrieveAPIView):
+    queryset = Art.objects.all()
+    serializer_class = ArtSerializer
+
+
+class GetARandomArtAPI(APIView):
+    def get(self, requests, *args, **kwargs):
+        queryset = random.choice(Art.objects.all())
+        return Response(ArtSerializer(queryset).data)
+
+
+class GetARandomArtPictureAPI(APIView):
+    def get(self, requests, *args, **kwargs):
+        queryset = random.choice(Art.objects.all())
+        return HttpResponseRedirect(ArtSerializer(queryset).data['picture'])
